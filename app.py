cf5d9eb909c7e4b7287fe0b3d7d15206a1891367
Karen Torres <kal.torreso@yaviracl.ec>
2026-06-29 15:53:46 -0500

    s

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ def home():
     return render_template_string(HTML_TEMPLATE)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
